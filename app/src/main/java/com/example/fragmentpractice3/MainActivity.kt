78ad6ef062c35eea97a3ad22679bdb0a7ce0600e
강진우 <wlsdn1252@naver.com>
2023-01-17 16:00:43 +0900

    어댑터 활용 뷰페이저에 연동, 화면 구현

--- app/src/main/java/com/example/fragmentpractice3/MainActivity.kt
+++ app/src/main/java/com/example/fragmentpractice3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.fragmentpractice3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentpractice3.adapters.MainAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAdapter = MainAdapter(supportFragmentManager)
+
+        mainViewPager.adapter = mAdapter
+
     }
 }
