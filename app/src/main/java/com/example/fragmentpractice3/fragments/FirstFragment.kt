40a6cb7a1633b009d5fa284fc38f1d881aca6cfd
강진우 <wlsdn1252@naver.com>
2023-01-17 15:56:51 +0900

    2개의 프래그먼트.xml에 대한 프래그먼트 코틀린 파일 작성

---
+++ app/src/main/java/com/example/fragmentpractice3/fragments/FirstFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentpractice3.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice3.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fist,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
